049562e06069b10f7b4a64c758cb5158b2bc7c26
Nguyen Quy <quynx9503.dev@gmail.com>
2025-05-12 23:26:28 +0700

    Initial Commit

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import path from "path";
+import { defineConfig } from "vite";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+
+export default defineConfig({
+    root: "src",
+    publicDir: "../public",
+    build: {
+        outDir: "../dist",
+        emptyOutDir: true, // Check if there are files from previous build then delete them and re-build a new one
+        rollupOptions: {
+            input: {
+                // Using __dirname for get the absolute path on this computer upto current directory
+                home: path.resolve(__dirname, "src/index.html"),
+                about: path.resolve(__dirname, "src/about.html"),
+            },
+        },
+    },
+    plugins: [ViteEjsPlugin()],
+});
